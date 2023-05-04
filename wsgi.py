1423eb692202cc84cd1fcb02e5a3a27c50d54d57
clovisphere <clovis.mugaruka@gmail.com>
2023-05-04 15:30:17 +0300

    Remove unused code

--- wsgi.py
+++ wsgi.py
@@ -4,9 +4,3 @@ from flask_migrate import Migrate
 
 app = create_app(os.environ.get('FLASK_ENV', 'default'))
 migrate = Migrate(app, db, include_schemas=True)
-
-
-def test():
-    """Run the unit tests."""
-    import pytest
-    pytest.main(['-s', './tests'])
